e03ac7947dd529bd8cf237deae63fe7806293063
rahul.rajput <rahul.rajput@achieve3000.com>
2020-10-31 07:53:51 +0530

    Add User model for login and sign

--- zenatixTask/models.py
+++ zenatixTask/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=255,unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    mobileNumber = models.CharField(null=True,max_length=18, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
